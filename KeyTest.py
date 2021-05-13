3ed66d5dc7ff75ae4dd754f2bad609702fb18c87
Conner <connertenn@gmail.com>
2021-05-13 01:20:46 -0400

    Added basics of Key object

--- KeyTest.py
+++ KeyTest.py
@@ -1,5 +1,5 @@
 import cadquery as cq
-import glm
+from glm import *
 
 #Stop VS-Code undefined function error
 if "show_object" not in globals():
@@ -8,3 +8,20 @@ if "show_object" not in globals():
     show_object = func
 
 
+class Key:
+    Length = 150
+    HiddenLength = 50
+    TotalLength = Length + HiddenLength
+    Width = 23.5
+    Height = 10
+    WallThickness = 2
+
+    def __new__(self):
+        key = cq.Workplane().box(self.Width, self.TotalLength, self.WallThickness)
+        key = key.translate((0, self.TotalLength/2 - self.Length, 0))
+
+        return key
+
+key = Key()
+
+show_object(key)
