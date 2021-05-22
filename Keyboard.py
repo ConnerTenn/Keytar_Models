d9d14011cbef6381ad60f5ad3e450a8a10306e25
Conner <connertenn@gmail.com>
2021-05-21 21:58:20 -0400

    Added initial elements of the key

--- Keyboard.py
+++ Keyboard.py
@@ -8,3 +8,59 @@ if "show_object" not in globals():
     show_object = func
 
 
+class Octave(object):
+    Width = 24*7
+
+    def __new__(self):
+        self = super().__new__(self)
+
+
+
+class KeyCommon(object):
+    MountLength = 40
+    HiddenLength = 50
+    Width = Octave.Width/12
+    Height = 10
+    WallThickness = 4
+
+    TotalLength = MountLength+HiddenLength
+
+    def __new__(self):
+        self = super().__new__(self)
+
+        common = cq.Workplane().box(self.Width, self.TotalLength, self.Height) \
+            .translate((0,self.TotalLength/2-self.MountLength,0))
+
+        common -= self.PivotCut()
+        common -= self.SpringCut()
+
+        return common
+
+
+    PivotPos = cq.Vector(25, 0) #offset from center
+
+    def PivotCut(self):
+        cut = cq.Workplane("YZ").move(self.PivotPos.x, self.PivotPos.y) \
+            .line(10,-10).line(-20,0).close() \
+            .extrude(self.Width/2, both=True)
+
+        # show_object(cut)
+
+        return cut
+
+
+    SpringDiameter = 3
+    SpringPos = HiddenLength-WallThickness-(SpringDiameter+1)/2-2
+
+    def SpringCut(self):
+        cut = cq.Workplane("XY") \
+            .move(self.SpringDiameter, self.SpringPos).circle((self.SpringDiameter+1)/2).mirrorY() \
+            .extrude(self.Height/2, both=True)
+
+        return cut
+
+
+common = KeyCommon()
+
+show_object(common)
+
